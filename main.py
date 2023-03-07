28bf387fde8207bdfa5a55fa05ce42bd37e0a43b
kpatzer <126931254+kpatzer@users.noreply.github.com>
2023-03-07 19:07:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Band Name Generator greeting
+print("Welcome to the Band Name Generator!\n")
+
+#Create populate two variables via input
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+
+#Concatenate city and name (space between) presenting band name to user
+print("Your band name could be " + city + " " + pet + ".")
